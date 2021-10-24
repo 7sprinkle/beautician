15e739a511cbf10a91e104f9cdf86ea5d70ec2ae
septem <qitengyuxi@gmail.com>
2021-10-24 16:57:16 +0900

    コメント削除追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,14 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
 
   private
 
